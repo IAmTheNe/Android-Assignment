61774ff6a3ca604151072c56a15b6240c64b636b
Trần Nguyên Thế <nguyenthe.tnt@gmail.com>
2022-01-13 09:40:57 +0700

    Tạo thông báo mỗi ngày lúc 6:30AM

---
+++ app/src/main/java/com/iamthene/driverassistant/fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,55 @@
+package com.iamthene.driverassistant.fcm;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.iamthene.driverassistant.R;
+import com.iamthene.driverassistant.activity.SplashActivity;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public static final String TAG = MyFirebaseMessagingService.class.getName();
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        if (notification == null) {
+            return;
+        }
+        String title = notification.getTitle();
+        String message = notification.getBody();
+
+        sendNotification(title, message);
+    }
+
+    private void sendNotification(String title, String message) {
+        Intent intent = new Intent(this, SplashActivity.class);
+        @SuppressLint("UnspecifiedImmutableFlag") PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, MyApplication.CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent);
+        Notification notification = notificationBuilder.build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            notificationManager.notify(1, notification);
+        }
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        Log.e(TAG, s);
+    }
+}
