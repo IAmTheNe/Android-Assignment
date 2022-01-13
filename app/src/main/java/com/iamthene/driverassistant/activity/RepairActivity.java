38a9c72639221a5c093054d3738b0d19ecdcbdaa
Trần Nguyên Thế <nguyenthe.tnt@gmail.com>
2022-01-13 13:03:47 +0700

    xuất file repair - android 11 không chạy được

--- app/src/main/java/com/iamthene/driverassistant/activity/RepairActivity.java
+++ app/src/main/java/com/iamthene/driverassistant/activity/RepairActivity.java
@@ -1,27 +1,50 @@
 package com.iamthene.driverassistant.activity;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.iamthene.driverassistant.R;
-import com.iamthene.driverassistant.fragment.EmptyFragment;
-import com.iamthene.driverassistant.fragment.RepairFragment;
-import com.iamthene.driverassistant.presenter.RepairInterface;
-import com.iamthene.driverassistant.presenter.RepairPresenter;
+import com.iamthene.driverassistant.adapter.RepairAdapter;
+import com.iamthene.driverassistant.model.Repair;
+import com.iamthene.driverassistant.utils.ExportFile;
 
-public class RepairActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener, RepairInterface.OnCheckEmptyList {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepairActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
     MaterialToolbar toolbar;
-    FrameLayout frlContent;
-    RepairPresenter mPresenter;
+    LinearLayout lyEmpty;
+    RecyclerView rvPartChange;
+    RepairAdapter adapter;
+    List<Repair> lstLK;
+    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference _myRef;
+    ArrayList<String> mKeys = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +52,95 @@ public class RepairActivity extends AppCompatActivity implements Toolbar.OnMenuI
         setContentView(R.layout.activity_repair);
 
         inIt();
-        initEvent();
+        getData();
+        adapter = new RepairAdapter(this, lstLK);
+        rvPartChange.setAdapter(adapter);
     }
 
     private void inIt() {
-        frlContent = findViewById(R.id.frlContent);
         toolbar = findViewById(R.id.toolbarOption3);
         toolbar.setOnMenuItemClickListener(this);
         toolbar.setNavigationOnClickListener(view -> {
             finish();
         });
-        mPresenter = new RepairPresenter(this);
+        rvPartChange = findViewById(R.id.rvPartChange);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        rvPartChange.setLayoutManager(linearLayoutManager);
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        lyEmpty = findViewById(R.id.lyEmpty);
+    }
+
+    private void getData() {
+        lstLK = new ArrayList<>();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        _myRef = mDatabase.getReference("Repair");
+        assert user != null;
+        _myRef.child(user.getUid()).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Repair repair = snapshot.getValue(Repair.class);
+                if (repair != null) {
+                    lstLK.add(repair);
+                    mKeys.add(snapshot.getKey());
+                    adapter.notifyDataSetChanged();
+                }
+
+                if (lstLK.isEmpty()) {
+                    lyEmpty.setVisibility(View.VISIBLE);
+                    rvPartChange.setVisibility(View.GONE);
+                } else {
+                    lyEmpty.setVisibility(View.GONE);
+                    rvPartChange.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Repair repair = snapshot.getValue(Repair.class);
+                if (repair == null || lstLK == null || lstLK.isEmpty()) {
+                    return;
+                }
+                String key = snapshot.getKey();
+                int index = mKeys.indexOf(key);
+                lstLK.set(index, repair);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                Repair repair = snapshot.getValue(Repair.class);
+                if (repair == null || lstLK == null || lstLK.isEmpty()) {
+                    return;
+                }
+                String key = snapshot.getKey();
+                int index = mKeys.indexOf(key);
+                if (index != -1) {
+                    lstLK.remove(index);
+                    mKeys.remove(index);
+                }
+
+                if (lstLK.isEmpty()) {
+                    lyEmpty.setVisibility(View.VISIBLE);
+                    rvPartChange.setVisibility(View.GONE);
+                } else {
+                    lyEmpty.setVisibility(View.GONE);
+                    rvPartChange.setVisibility(View.VISIBLE);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
@@ -49,31 +150,20 @@ public class RepairActivity extends AppCompatActivity implements Toolbar.OnMenuI
             Intent intent = new Intent(this, NewRepairActivity.class);
             startActivity(intent);
         }
+        if (id == R.id.mnuExport) {
+            if (lstLK.isEmpty()) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Xuất file")
+                        .setMessage("Hiện tại không có dữ liệu để xuất file")
+                        .setNegativeButton("OK", null)
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+                ExportFile exportFile = new ExportFile();
+                exportFile.exportRepair(this, "Thống kê thay linh kiện", lstLK);
+            }
+        }
         return false;
-    }
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frlContent, fragment);
-        transaction.commitAllowingStateLoss();
-    }
-
-    private void initEvent() {
-        mPresenter.isEmptyList();
-    }
-
-    @Override
-    public void empty() {
-        replaceFragment(new EmptyFragment());
-    }
-
-    @Override
-    public void exists() {
-        replaceFragment(new RepairFragment());
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
     }
 }
---
+++ app/src/main/java/com/iamthene/driverassistant/utils/ExportFile.java
@@ -0,0 +1,68 @@
+package com.iamthene.driverassistant.utils;
+
+import android.content.Context;
+import android.os.Environment;
+import android.widget.Toast;
+
+import com.iamthene.driverassistant.model.Repair;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.WorkbookUtil;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
+
+public class ExportFile {
+    public void exportRepair(Context context, String name, List<Repair> repairs) {
+        Workbook wb2003 = new HSSFWorkbook();
+        String safeName = WorkbookUtil.createSafeSheetName(name);
+        Sheet sheet = wb2003.createSheet(safeName);
+
+        // Khỏi tạo Row, Cell đầu tiên
+        Row row = sheet.createRow(0);
+        Cell cell = row.createCell(0);
+        cell.setCellValue("Thời gian");
+        cell = row.createCell(1);
+        cell.setCellValue("Tên phương tiện");
+        cell = row.createCell(2);
+        cell.setCellValue("Phụ tùng");
+        cell = row.createCell(3);
+        cell.setCellValue("Chi phí");
+
+        // Đổ dữ liệu
+        int numRow = 1;
+        for (Repair r : repairs) {
+            row = sheet.createRow(numRow);
+            cell = row.createCell(0);
+            cell.setCellValue(r.getDate().concat(" ").concat(r.getTime()));
+            cell = row.createCell(1);
+            cell.setCellValue(r.getCarId());
+            cell = row.createCell(2);
+            cell.setCellValue(r.getPart());
+            cell = row.createCell(3);
+            cell.setCellValue(r.getPrice());
+            numRow++;
+        }
+
+        // Xuất file
+        File filePath = new File(Environment.getExternalStorageDirectory() + "/Thống kê sửa xe.xls");
+        try {
+            if (!filePath.exists()) {
+                filePath.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(filePath);
+            wb2003.write(fos);
+            Toast.makeText(context, "Xuất file thành công!", Toast.LENGTH_SHORT).show();
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            Toast.makeText(context, "Xuất file thất bại!", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+}
